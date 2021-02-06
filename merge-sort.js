6c67b931f72e5e9fb2b15e841c081bcef37a4348
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-02-06 16:25:14 -0500

    adding quick sort

--- merge-sort.js
+++ merge-sort.js
@@ -47,3 +47,47 @@ left: 46,
 right: 40
 
 */
+
+function mergeSort(array) {
+  console.log('MergeSort ran')
+  if (array.length <= 1) {
+      return array;
+  }
+
+  const middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle, array.length);
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+  return merge(left, right, array);
+};
+
+function merge(left, right, array) {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+      console.log('Merge ran')
+      if (left[leftIndex] < right[rightIndex]) {
+          array[outputIndex++] = left[leftIndex++];
+      }
+      else {
+          array[outputIndex++] = right[rightIndex++];
+      }
+  }
+
+  for (let i = leftIndex; i < left.length; i++) {
+      array[outputIndex++] = left[i];
+  }
+
+  for (let i = rightIndex; i < right.length; i++) {
+      array[outputIndex++] = right[i];
+  }
+  return array;
+};
+
+const anotherArr = [38, 27, 43, 3, 9, 82, 10];
+const array = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40]
+console.log(mergeSort(array));
+console.log(mergeSort(anotherArr));
